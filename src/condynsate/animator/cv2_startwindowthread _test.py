41a64f907c97488b959fcb338497c34fd8080187
GrayKS3248 <gschaer2@illinois.edu>
2025-09-29 12:58:56 -0500

    cv2 thread test

--- src/condynsate/animator/cv2_startwindowthread _test.py
+++ src/condynsate/animator/cv2_startwindowthread _test.py
@@ -6,18 +6,18 @@ from threading import (Thread, Lock)
 
 
 class Test:
-    def __init__(self, name='TEST', pix=(0.1, 0.5, 0.9), frame_rate=5.0):
-        self.name = name
-        self.pix = pix
+    def __init__(self, num=0, frame_rate=5.0):
+        self.name = 'Animator'
+        self.num = num
         self.frame_rate = frame_rate
         
         self._lock = Lock()
         
         self._done = True
         self._prev_draw_time = time.time()
-        self._prev_draw_pix = copy(self.pix)
+        self._prev_num = copy(self.num)
         
-        self._img = np.zeros((240, 320, 3), dtype=np.uint8)
+        self._img = np.zeros((480, 640, 3), dtype=np.uint8)
         self._draw_image()
         self._update_ready = True
     
@@ -25,21 +25,35 @@ class Test:
         self.stop()
     
     def _draw_image(self):
-        self._img[:,:,0] = np.clip(self.pix[0]*255., 0., 255.).astype(np.uint8)
-        self._img[:,:,1] = np.clip(self.pix[1]*255., 0., 255.).astype(np.uint8)
-        self._img[:,:,2] = np.clip(self.pix[2]*255., 0., 255.).astype(np.uint8)
+        self._img[:,:,0] = np.uint8(0)
+        self._img[:,:,1] = np.uint8(0)
+        self._img[:,:,2] = np.uint8(0)
+        
+        text = str(self.num)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 5
+        thickness = 15
+        color = (255, 255, 255)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        
+        (w, h), baseline = cv2.getTextSize(text, font, font_scale, thickness)
+        x = int(np.round(0.5 * (640 - w)))
+        y = int(np.round(0.5 * (480 - h) + h))
+        
+        self._img = cv2.putText(self._img, text, (x, y), font, font_scale, 
+                                color, thickness, cv2.LINE_AA, False)
         self._update_ready = True
     
     def _start_drawer(self):
         while True:
             c1 = time.time() - self._prev_draw_time >= 1.0 / self.frame_rate
-            c2 = not self.pix == self._prev_draw_pix
+            c2 = not self.num == self._prev_num
             
             if c1 and c2:
                 with self._lock:
                     self._draw_image()
                     self._prev_draw_time = time.time()
-                    self._prev_draw_pix = copy(self.pix)
+                    self._prev_num = copy(self.num)
                     
             else:
                 time.sleep(0.01 * (1.0 / self.frame_rate))
@@ -53,18 +67,17 @@ class Test:
         cv2.waitKey(1)
     
     def start(self):
-        cv2.startWindowThread()
         cv2.namedWindow(self.name, cv2.WINDOW_AUTOSIZE)
         self._done = False
         time.sleep(0.1)
-        self.update_image(self.pix)
+        self.update_image(self.num)
         self._thread = Thread(target=self._start_drawer)
         self._thread.daemon = True
         self._thread.start()
     
-    def update_image(self, pix):
+    def update_image(self, num):
         with self._lock:
-            self.pix = pix
+            self.num = num
             if not self._done and self._update_ready:
                 cv2.imshow(self.name, self._img)
                 cv2.waitKey(1)
@@ -84,7 +97,6 @@ class Test:
 if __name__ == "__main__":
     test = Test()
     test.start()
-    for i in range(100):
-        pix = (np.random.rand(), np.random.rand(), np.random.rand())
-        test.update_image(pix)
+    for i in range(300):
+        test.update_image(i+1)
     test.stop()
