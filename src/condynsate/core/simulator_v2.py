63917e19b162e51c242cfdf18ad6dfae445dfc80
GrayKS3248 <gschaer2@illinois.edu>
2025-11-17 11:56:29 -0600

    add sim objects, start

--- src/condynsate/core/simulator_v2.py
+++ src/condynsate/core/simulator_v2.py
@@ -10,62 +10,436 @@ simulations using the PyBullet package.
 #DEPENDENCIES
 ###############################################################################
 from dataclasses import dataclass
+from warnings import warn
+import numpy as np
 import pybullet
 from pybullet_utils import bullet_client as bc
+import condynsate.core.transforms as t
 
-###############################################################################
-#SIMULATOR OBJECT STATE DATACLASS
-###############################################################################
-@dataclass
-class _State():
-    """
-    Stores state information.
-    """
-    position: tuple = (0.0, 0.0, 0.0)
-    velocity: tuple = (0.0, 0.0, 0.0)
-    orientation: tuple = (1.0, 0.0, 0.0, 0.0)
-    omega: tuple = (0.0, 0.0, 0.0)
-    
-    def set_position(self, position):
-        self.position = tuple(p for i,p in enumerate(position) if i < 3)
-        
-    def set_position(self, position):
-        self.position = tuple(p for i,p in enumerate(position) if i < 3)
-        
-    def set_position(self, position):
-        self.position = tuple(p for i,p in enumerate(position) if i < 3)
-            
-        def set_position(self, position):
-            self.position = tuple(p for i,p in enumerate(position) if i < 3)
-
-@dataclass 
-class Sim_Obj_State(_State):
-    """
-    Stores current state information about simulator object.
-    """
-
-@dataclass
-class Sim_Obj_Init_State(_State):
-    """
-    Stores initial state information about simulator object.
-    """
-    
 ###############################################################################
 #SIMULATOR OBJECT CLASS
 ###############################################################################
+@dataclass
+class Base_State():
+    """
+    Stores state information about the base of an object.
+
+    Parameters
+    ----------
+    **kwargs :
+        State information with the following acceptable keys
+        position : 3 tuple of floats, optional
+            The XYZ position in world coordinates.
+            The default is (0., 0., 0.)
+        orientation : 4 tuple of floats, optional
+            The wxyz quaternion representation of the orientation in world
+            coordinates. The default is (1., 0., 0., 0.)
+        velocity : 3 tuple of floats, optional
+            The XYZ velocity in either world or body coordinates. Body
+            coordinates are defined based on objects orientation.
+            The default is (0., 0., 0.)
+        omega : 3 tuple of floats, optional
+            The XYZ angular velocity in either world or body coordinates.
+            Body coordinates are defined based on objects orientation.
+            The default is (0., 0., 0.)
+        body : bool, optional
+            Whether velocity and omega are being set in world or body
+            coordinates. The default is False
+    """
+    _position: tuple = (0.0, 0.0, 0.0)
+    _orientation: tuple = (1.0, 0.0, 0.0, 0.0)
+    _velocity: tuple = (0.0, 0.0, 0.0)
+    _omega: tuple = (0.0, 0.0, 0.0)
+
+    def __init__(self, **kwargs):
+        body = kwargs.get('body', False)
+        if 'position' in kwargs:
+            self.position = kwargs['position']
+        if 'orientation' in kwargs:
+            self.orientation = kwargs['orientation']
+        if 'velocity' in kwargs:
+            if body:
+                self.set_velocity_in_body(kwargs['velocity'])
+            else:
+                self.velocity = kwargs['velocity']
+        if 'omega' in kwargs:
+            if body:
+                self.set_omega_in_body(kwargs['omega'])
+            else:
+                self.omega = kwargs['omega']
+
+    @property
+    def position(self):
+        """ XYZ position world coordinates """
+        return self._position
+
+    @property
+    def orientation(self):
+        """ WXYZ quaternion orientation in world coordinates """
+        return self._orientation
+
+    @property
+    def velocity(self):
+        """ XYZ velocity in world coordinates """
+        return self._velocity
+
+    @property
+    def omega(self):
+        """ XYZ angular velocity in world coordinates """
+        return self._omega
+
+    @position.setter
+    def position(self, position):
+        try:
+            self._position = tuple(float(p) for p in position)
+        except (TypeError, ValueError):
+            warn(f"Unable to set position to {position}.")
+
+    @orientation.setter
+    def orientation(self, orientation):
+        try:
+            q_0 = np.array([q for i,q in enumerate(orientation) if i < 4])
+            self._orientation = tuple((q_0 / np.linalg.norm(q_0)).tolist())
+        except (TypeError, ValueError):
+            warn(f"Unable to set orientation to {orientation}.")
+        except ZeroDivisionError:
+            warn(f"Unable to set orientation to {orientation}, magnitude 0.")
+
+    @velocity.setter
+    def velocity(self, velocity):
+        try:
+            self._velocity = tuple(float(v) for v in velocity)
+        except (TypeError, ValueError):
+            warn(f"Unable to set velocity to {velocity}.")
+
+    @omega.setter
+    def omega(self, omega):
+        try:
+            self._omega = tuple(float(w) for w in omega)
+        except (TypeError, ValueError):
+            warn(f"Unable to set omega to {omega}.")
+
+    def get_velocity_in_body(self):
+        """
+        Gets the velocity in body coordinates.
+
+        Returns
+        -------
+        velocity_in_body : 3 tuple of floats
+            The velocity in body coordinates as defined by the orientation.
+
+        """
+        Rbw = t.Rbw_from_wxyz(self.orientation)
+        Rwb = t.Rab_to_Rba(Rbw)
+        return tuple(t.va_to_vb(Rwb, self.velocity).tolist())
+
+    def get_omega_in_body(self):
+        """
+        Gets the angular velocity in body coordinates.
+
+        Returns
+        -------
+        omega_in_body : 3 tuple of floats
+            The angular velocity in body coordinates as defined by the
+            orientation.
+
+        """
+        Rbw = t.Rbw_from_wxyz(self.orientation)
+        Rwb = t.Rab_to_Rba(Rbw)
+        return tuple(t.va_to_vb(Rwb, self.omega).tolist())
+
+    def set_velocity_in_body(self, velocity):
+        """
+        Sets the velocity where the passed velocity argumnet is defined in
+        body coordinates as defined by the orientation.
+
+        Parameters
+        ----------
+        velocity : 3 tuple of floats
+            The velocity to set in body coordinates.
+
+        Returns
+        -------
+        None.
+
+        """
+        try:
+            Rbw = t.Rbw_from_wxyz(self.orientation)
+            self._velocity = tuple(t.va_to_vb(Rbw, velocity).tolist())
+        except (TypeError, ValueError):
+            warn(f"Unable to set velocity to {velocity}.")
+
+    def set_omega_in_body(self, omega):
+        """
+        Sets the velocity where the passed velocity argumnet is defined in
+        body coordinates as defined by the orientation.
+
+        Parameters
+        ----------
+        velocity : 3 tuple of floats
+            The velocity to set in body coordinates.
+
+        Returns
+        -------
+        None.
+
+        """
+        try:
+            Rbw = t.Rbw_from_wxyz(self.orientation)
+            self._omega = tuple(t.va_to_vb(Rbw, omega).tolist())
+        except (TypeError, ValueError):
+            warn(f"Unable to set omega to {omega}.")
+
 class Sim_Obj():
-    
-    def __init__(self):
-        pass
+    """
+    """
+    def __init__(self, client, path, **kwargs):
+        self._client = client
+        self._init_base_state = Base_State(**kwargs)
+        self._id = self._load_urdf(path, **kwargs)
+        self._links, self._joints = self._build_links_and_joints()
+
+    def _load_urdf(self, urdf_path, **kwargs):
+        # Use implicit cylinder for collision and physics calculation
+        # Specifies to the engine to use the inertia from the urdf file
+        f1 = self._client.URDF_USE_IMPLICIT_CYLINDER
+        f2 = self._client.URDF_USE_INERTIA_FROM_FILE
+
+        # Load the URDF
+        flags = f1 | f2
+        basePosition = self._init_base_state.position
+        baseOrientation = self._init_base_state.orientation
+        useFixedBase = kwargs.get('fixed', False)
+        urdf_id = self._client.loadURDF(urdf_path,
+                                        flags=flags,
+                                        basePosition=basePosition,
+                                        baseOrientation=baseOrientation,
+                                        useFixedBase=useFixedBase)
+
+        # Set the base velocity of the URDF
+        linearVelocity = self._init_base_state.velocity
+        angularVelocity = self._init_base_state.omega
+        self._client.resetBaseVelocity(objectUniqueId=urdf_id,
+                                       linearVelocity=linearVelocity,
+                                       angularVelocity=angularVelocity)
+
+        return urdf_id
+
+    def _build_links_and_joints(self):
+        for joint_id in range(self._client.getNumJoints(self._id)):
+            info = self._client.getJointInfo(self._id, joint_id)
+
+    def set_base_state(self, **kwargs):
+        """
+        Sets the base state of the object.
+
+        Parameters
+        ----------
+        **kwargs
+            State information with the following acceptable keys
+            position : 3 tuple of floats, optional
+                The XYZ position in world coordinates.
+                The default is (0., 0., 0.)
+            yaw : float, optional
+                The (Tait–Bryan) yaw angle of the object in radians.
+            pitch : float, optional
+                The (Tait–Bryan) pitch angle of the object in radians.
+            roll : float, optional
+                The (Tait–Bryan) roll angle of the object in radians.
+            velocity : 3 tuple of floats, optional
+                The XYZ velocity in either world or body coordinates. Body
+                coordinates are defined based on object's orientation.
+                The default is (0., 0., 0.)
+            omega : 3 tuple of floats, optional
+                The XYZ angular velocity in either world or body coordinates.
+                Body coordinates are defined based on object's orientation.
+                The default is (0., 0., 0.)
+            body : bool, optional
+                Whether velocity and omega are being set in world or body
+                coordinates. The default is False
+
+        Returns
+        -------
+        ret_code : int
+            0 if successful, -1 if something went wrong.
+
+        """
+        # Update the stored base state
+        if 'position' in kwargs:
+            self._base_state.position = kwargs['position']
+        if 'yaw' in kwargs or 'pitch' in kwargs or 'roll' in kwargs:
+            y0, p0, r0 = t.euler_from_wxyz(self._base_state.orientation)
+            yaw = kwargs.get('yaw', y0)
+            pitch = kwargs.get('pitch', p0)
+            roll = kwargs.get('roll', r0)
+            orientation = t.wxyz_from_euler(yaw, pitch, roll)
+            self._base_state.orientation = orientation
+        if 'velocity' in kwargs:
+            if kwargs.get('body', False):
+                self._base_state.set_velocity_in_body(kwargs['velocity'])
+            else:
+                self._base_state.velocity = kwargs['velocity']
+        if 'omega' in kwargs:
+            if kwargs.get('body', False):
+                self._base_state.set_omega_in_body(kwargs['omega'])
+            else:
+                self._base_state.omega = kwargs['omega']
+
+        # Send the updated state to the physics client
+        posObj = self._base_state.position
+        ornObj = self._base_state.orientation
+        self._client.resetBasePositionAndOrientation(bodyUniqueId=self._id,
+                                                     posObj=posObj,
+                                                     ornObj=ornObj)
+        linearVelocity = self._base_state.velocity
+        angularVelocity = self._base_state.omega
+        self._client.resetBaseVelocity(objectUniqueId=self._id,
+                                       linearVelocity=linearVelocity,
+                                       angularVelocity=angularVelocity)
+        return 0
+
+    def get_base_state(self, body=False):
+        """
+        Gets the base state of the object.
+
+        Parameters
+        ----------
+        body : bool, optional
+            A boolean flag that determines if the fetch velocity and angular
+            velocity states are in body coordinate (True) or world coordinates
+            (False). The default is False.
+
+        Returns
+        -------
+        base_state : dict
+            A Dictionary containing the state information. The keys are
+            position, yaw, pitch, roll, velocity, and omega.
+
+        """
+        position = self._base_state.position
+        yaw, pitch, roll = t.euler_from_wxyz(self._base_state.orientation)
+        if body:
+            velocity = self._base_state.get_velocity_in_body()
+            omega = self._base_state.get_omega_in_body()
+        else:
+            velocity = self._base_state.velocity
+            omega = self._base_state.omega
+        base_state = {'position' : position,
+                      'yaw' : yaw,
+                      'pitch' : pitch,
+                      'roll' : roll,
+                      'velocity' : velocity,
+                      'omega' : omega,}
+        return base_state
+
+###############################################################################
+#JOINT CLASS
+###############################################################################
+@dataclass
+class Joint_State():
+    """
+    Stores state information about the base of an object.
+
+    Parameters
+    ----------
+    **kwargs :
+        State information with the following acceptable keys
+        angle : float, optional
+            The angle of the joint about the joint axis. The default is 0.
+        omega : float, optional
+            The angular velocity of the joint about the joint axis.
+            The default is 0.
+    """
+    _angle: float = 0.0
+    _omega: float = 0.0
+
+    def __init__(self, **kwargs):
+        if 'angle' in kwargs:
+            self.angle = kwargs['angle']
+        if 'omega' in kwargs:
+            self.omega = kwargs['omega']
+
+    @property
+    def angle(self):
+        """ The joint angle about the joint axis """
+        return self._angle
+
+    @property
+    def omega(self):
+        """ The joint angular velocity about the joint axis """
+        return self._omega
+
+    @angle.setter
+    def angle(self, angle):
+        try:
+            self._angle = float(angle)
+        except (TypeError, ValueError):
+            warn(f"Unable to set angle to {angle}.")
+
+    @omega.setter
+    def omega(self, omega):
+        try:
+            self._omega = float(omega)
+        except (TypeError, ValueError):
+            warn(f"Unable to set omega to {omega}.")
+
+class Joint:
+    """
+    """
+    def __init__(self, client):
+        self._client = client
+
+    def set_dynamics(self, **kwargs):
+        args = {}
+        if 'joint_resistance' in kwargs:
+            args['jointDamping'] = kwargs['joint_resistance']
+        if 'max_omega' in kwargs:
+            args['maxJointVelocity'] = kwargs['max_omega']
+
+        if 'mass' in kwargs:
+            args['mass'] = kwargs['mass']
+        if 'lateral_contact_friction' in kwargs:
+            args['lateralFriction'] = kwargs['lateral_contact_friction']
+        if 'spinning_contact_friction' in kwargs:
+            args['spinningFriction'] = kwargs['spinning_contact_friction']
+        if 'rolling_contact_friction' in kwargs:
+            args['rollingFriction'] = kwargs['rolling_contact_friction']
+        if 'bounciness' in kwargs:
+            args['restitution'] = kwargs['bounciness']
+        if 'linear_air_resistance' in kwargs:
+            args['linearDamping'] = kwargs['linear_air_resistance']
+        if 'angular_air_resistance' in kwargs:
+            args['angularDamping'] = kwargs['angular_air_resistance']
+        if 'contact_damping' in kwargs:
+            args['contactDamping'] = kwargs['contact_damping']
+        if 'contact_stiffness' in kwargs:
+            args['contactStiffness'] = kwargs['contact_stiffness']
+
+        urdf_id = 0
+        link_id = 0
+        self._client.changeDynamics(urdf_id, link_id, **args)
+
+        return 0
+
+        # mode = self.engine.VELOCITY_CONTROL
+        # self.engine.setJointMotorControlArray(urdf_id,
+        #                                       joint_id,
+        #                                       mode,
+        #                                       forces=[0])
+
+        # self.engine.enableJointForceTorqueSensor(urdf_id,
+        #                                          joint_id,
+        #                                          enable_sensor)
 
 ###############################################################################
 #SIMULATOR CLASS
 ###############################################################################
 class Simulator():
-
+    """
+    """
     def __init__(self, gravity=(0.0, 0.0, -9.81), dt=0.01):
         # Start engine and client in direct mode (no visualization)
-        self._client = bc.BulletClient(connection_mode=pybullet.DIRECT)
+        self._client = bc.BulletClient(connection_mode=pybullet.GUI)
         self.set_gravity(gravity)
         client_params = {
                         'fixedTimeStep' : dt,
@@ -99,6 +473,17 @@ class Simulator():
         """
         self._client.setGravity(gravity[0], gravity[1], gravity[2])
         return 0
+
+    def load_urdf(self, path, **kwargs):
+        # Convert the euler angles to quaternion
+        yaw = kwargs.get('yaw', 0.0)
+        pitch = kwargs.get('pitch', 0.0)
+        roll = kwargs.get('roll', 0.0)
+        kwargs['orientation'] = t.wxyz_from_euler(yaw, pitch, roll)
+
+        # Build the object and return it
+        obj = Sim_Obj(self._client, path, **kwargs)
+        return obj
 
     def terminate(self):
         """
--- src/condynsate/core/transforms.py
+++ src/condynsate/core/transforms.py
@@ -8,7 +8,7 @@ This modules provides transform functions that are used by the simulator.
 ###############################################################################
 #DEPENDENCIES
 ###############################################################################
-import numpy as np
+import numpy as _np
 
 ###############################################################################
 #TRANSFORM FUNCTIONS
@@ -33,16 +33,16 @@ def xyzw_from_vecs(vec1, vec2):
 
     """
     # Convert to numpy array
-    arr1 = np.array(vec1)
-    arr2 = np.array(vec2)
+    arr1 = _np.array(vec1)
+    arr2 = _np.array(vec2)
 
     # Calculate the norm of vec
-    mag1 = np.linalg.norm(arr1)
-    mag2 = np.linalg.norm(arr2)
+    mag1 = _np.linalg.norm(arr1)
+    mag2 = _np.linalg.norm(arr2)
 
     # If either magnitude is 0, no rotation can be found.
     if mag1==0. or mag2==0.:
-        return np.array([0., 0., 0., 1.])
+        return _np.array([0., 0., 0., 1.])
 
     # If the magnitude is not zero, get the direction of vec
     dirn1 = arr1/mag1
@@ -50,13 +50,13 @@ def xyzw_from_vecs(vec1, vec2):
 
     # If the vec is exactly 180 degrees away, set the 180 deg quaternion
     if (dirn2==-1*dirn1).all():
-        return np.array([0.5*np.sqrt(2), -0.5*np.sqrt(2), 0., 0.])
+        return _np.array([0.5*_np.sqrt(2), -0.5*_np.sqrt(2), 0., 0.])
 
     # If the vec is some other relative orientation, calculate it
-    q_xyz = np.cross(dirn1, dirn2)
-    q_w = 1.0 + np.dot(dirn1, dirn2)
-    xyzw = np.append(q_xyz, q_w)
-    xyzw = xyzw/np.linalg.norm(xyzw)
+    q_xyz = _np.cross(dirn1, dirn2)
+    q_w = 1.0 + _np.dot(dirn1, dirn2)
+    xyzw = _np.append(q_xyz, q_w)
+    xyzw = xyzw/_np.linalg.norm(xyzw)
     return xyzw
 
 def xyzw_to_wxyz(xyzw):
@@ -74,7 +74,7 @@ def xyzw_to_wxyz(xyzw):
         The Hamilton representation of the input JPL quaterion
 
     """
-    return np.array([xyzw[3], xyzw[0], xyzw[1], xyzw[2]])
+    return _np.array([xyzw[3], xyzw[0], xyzw[1], xyzw[2]])
 
 def wxyz_to_xyzw(wxyz):
     """
@@ -91,7 +91,7 @@ def wxyz_to_xyzw(wxyz):
         The JPL representation of the input Hamilton quaterion.
 
     """
-    return np.array([wxyz[1], wxyz[2], wxyz[3], wxyz[0]])
+    return _np.array([wxyz[1], wxyz[2], wxyz[3], wxyz[0]])
 
 def xyzw_mult(q1, q2):
     """
@@ -146,20 +146,20 @@ def wxyz_mult(q1, q2):
     q3x = a2*b1 + b2*a1 + c2*d1 - d2*c1
     q3y = a2*c1 - b2*d1 + c2*a1 + d2*b1
     q3z = a2*d1 + b2*c1 - c2*b1 + d2*a1
-    return np.array([q3w, q3x, q3y, q3z])
+    return _np.array([q3w, q3x, q3y, q3z])
 
 def wxyz_from_euler(yaw, pitch, roll):
     """
-    Converts Euler angles to a Hamilton quaternion (wxyz)
+    Converts (Tait–Bryan) Euler Angles to a Hamilton quaternion (wxyz)
 
     Parameters
     ----------
     yaw : float
-        The roll angle in rad.
+        The yaw angle in rad.
     pitch : float
         The pitch angle in rad.
     roll : float
-        The yaw angle in rad.
+        The roll angle in rad.
 
     Returns
     -------
@@ -167,17 +167,47 @@ def wxyz_from_euler(yaw, pitch, roll):
         The Hamilton quaternion representation of the input Euler angles.
 
     """
-    cr = np.cos(roll * 0.5)
-    sr = np.sin(roll * 0.5)
-    cp = np.cos(pitch * 0.5)
-    sp = np.sin(pitch * 0.5)
-    cy = np.cos(yaw * 0.5)
-    sy = np.sin(yaw * 0.5)
+    cr = _np.cos(roll * 0.5)
+    sr = _np.sin(roll * 0.5)
+    cp = _np.cos(pitch * 0.5)
+    sp = _np.sin(pitch * 0.5)
+    cy = _np.cos(yaw * 0.5)
+    sy = _np.sin(yaw * 0.5)
     w = cr * cp * cy + sr * sp * sy
     x = sr * cp * cy - cr * sp * sy
     y = cr * sp * cy + sr * cp * sy
     z = cr * cp * sy - sr * sp * cy
-    return np.array([w, x, y, z])
+    return _np.array([w, x, y, z])
+
+def euler_from_wxyz(wxyz):
+    """
+    Converts a Hamilton quaternion (wxyz) to (Tait–Bryan) Euler Angles.
+
+    Parameters
+    ----------
+    wxyz : array-like, shape (4,)
+        The wxyz quaternion being converted.
+
+    Returns
+    -------
+    yaw : float
+        The equivalent yaw angle in rad.
+    pitch : float
+        The equivalent pitch angle in rad.
+    roll : float
+        The equivalent roll angle in rad.
+
+    """
+    w = wxyz[0]
+    x = wxyz[1]
+    y = wxyz[2]
+    z = wxyz[3]
+    roll = _np.arctan2(2.*(w*x+y*z), 1.-2.*(x*x+y*y))
+    p1 = _np.sqrt(1.+2.*(w*y-x*z))
+    p2 = _np.sqrt(1.-2.*(w*y-x*z))
+    pitch = 2.*_np.arctan2(p1, p2) - 0.5*_np.pi
+    yaw = _np.arctan2(2.*(w*z+x*y), 1.-2.*(y*y+z*z))
+    return yaw, pitch, roll
 
 def Rbw_from_wxyz(wxyz):
     """
@@ -194,10 +224,14 @@ def Rbw_from_wxyz(wxyz):
         The equivalent rotation matrix.
 
     """
+    # Trivial case
+    if all(_np.isclose(wxyz, [1., 0., 0., 0.])):
+        return _np.eye(3)
+
     # Ensure the quat's norm is greater than 0
-    s = np.linalg.norm(wxyz)
+    s = _np.linalg.norm(wxyz)
     if s == 0.0:
-        return np.eye(4)
+        return _np.eye(4)
     s = s**-2
 
     # Extract the values from Q
@@ -222,24 +256,24 @@ def Rbw_from_wxyz(wxyz):
     r22 = 1. - 2.*s*(qi*qi + qj*qj)
 
     # Build the rotation matrix
-    Rbw = np.array([[r00, r01, r02],
-                    [r10, r11, r12],
-                    [r20, r21, r22]])
+    Rbw = _np.array([[r00, r01, r02],
+                     [r10, r11, r12],
+                     [r20, r21, r22]])
     return Rbw
 
 def Rbw_from_euler(yaw, pitch, roll):
     """
-    Gets the orientation of a body in world coordinates from the euler angles
-    of the body.
+    Gets the orientation of a body in world coordinates from the (Tait–Bryan)
+    Euler Angles of the body.
 
     Parameters
     ----------
     yaw : float
-        The roll angle in rad.
+        The yaw angle in rad.
     pitch : float
         The pitch angle in rad.
     roll : float
-        The yaw angle in rad.
+        The roll angle in rad.
 
     Returns
     -------
@@ -248,21 +282,21 @@ def Rbw_from_euler(yaw, pitch, roll):
         takes vectors in body coordinates to world coordinates .
 
     """
-    cx = np.cos(roll)
-    sx = np.sin(roll)
-    cy = np.cos(pitch)
-    sy = np.sin(pitch)
-    cz = np.cos(yaw)
-    sz = np.sin(yaw)
-    Rx = np.array([[1., 0., 0.],
-                   [0., cx, -sx],
-                   [0., sx, cx]])
-    Ry = np.array([[cy, 0., sy],
-                   [0., 1., 0.],
-                   [-sy, 0., cy]])
-    Rz = np.array([[cz, -sz, 0.],
-                   [sz, cz, 0.],
-                   [0., 0., 1.]])
+    cx = _np.cos(roll)
+    sx = _np.sin(roll)
+    cy = _np.cos(pitch)
+    sy = _np.sin(pitch)
+    cz = _np.cos(yaw)
+    sz = _np.sin(yaw)
+    Rx = _np.array([[1., 0., 0.],
+                    [0., cx, -sx],
+                    [0., sx, cx]])
+    Ry = _np.array([[cy, 0., sy],
+                    [0., 1., 0.],
+                    [-sy, 0., cy]])
+    Rz = _np.array([[cz, -sz, 0.],
+                    [sz, cz, 0.],
+                    [0., 0., 1.]])
     return Rz@Ry@Rx
 
 def Rab_to_Rba(Rab):
@@ -281,7 +315,7 @@ def Rab_to_Rba(Rab):
         The rotation cosine matrix of the b frame in a coords.
 
     """
-    return np.array(Rab).T
+    return _np.array(Rab).T
 
 def Oab_to_Oba(Rab, Oab):
     """
@@ -301,7 +335,7 @@ def Oab_to_Oba(Rab, Oab):
         The origin of frame b in a coords.
 
     """
-    return -Rab_to_Rba(Rab) @ np.array(Oab)
+    return -Rab_to_Rba(Rab) @ _np.array(Oab)
 
 def va_to_vb(Rab, va):
     """
@@ -321,7 +355,7 @@ def va_to_vb(Rab, va):
         The same 3 vector in frame b coords.
 
     """
-    return np.array(Rab) @ np.array(va)
+    return _np.array(Rab) @ _np.array(va)
 
 def pa_to_pb(Rab, Oab, pa):
     """
@@ -343,4 +377,4 @@ def pa_to_pb(Rab, Oab, pa):
         The same 3D point in b coords.
 
     """
-    return np.array(Rab) @ np.array(pa) + np.array(Oab)
+    return _np.array(Rab) @ _np.array(pa) + _np.array(Oab)
